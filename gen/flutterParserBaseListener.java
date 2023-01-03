06c5aa3eec51540cf538687ac344829c7d976450
Bayan mansour <bayannmansourr@gmail.com>
2023-01-03 15:24:29 +0300

    add models

---
+++ gen/flutterParserBaseListener.java
@@ -0,0 +1,410 @@
+// Generated from C:/Users/Bayan/Desktop/CompilerProject3/src\flutterParser.g4 by ANTLR 4.10.1
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link flutterParserListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class flutterParserBaseListener implements flutterParserListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterProg(flutterParser.ProgContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitProg(flutterParser.ProgContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterWidgets(flutterParser.WidgetsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitWidgets(flutterParser.WidgetsContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTextField(flutterParser.TextFieldContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTextField(flutterParser.TextFieldContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTextFieldProperties(flutterParser.TextFieldPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTextFieldProperties(flutterParser.TextFieldPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTextFieldTextProperty(flutterParser.TextFieldTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTextFieldTextProperty(flutterParser.TextFieldTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTextFieldControllerProperty(flutterParser.TextFieldControllerPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTextFieldControllerProperty(flutterParser.TextFieldControllerPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTextFieldDecorationProperty(flutterParser.TextFieldDecorationPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTextFieldDecorationProperty(flutterParser.TextFieldDecorationPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInputDecorationProperties(flutterParser.InputDecorationPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInputDecorationProperties(flutterParser.InputDecorationPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInputDecorationLabelTextProperty(flutterParser.InputDecorationLabelTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInputDecorationLabelTextProperty(flutterParser.InputDecorationLabelTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInputDecorationHintTextProperty(flutterParser.InputDecorationHintTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInputDecorationHintTextProperty(flutterParser.InputDecorationHintTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInputDecorationHelperTextProperty(flutterParser.InputDecorationHelperTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInputDecorationHelperTextProperty(flutterParser.InputDecorationHelperTextPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterInputDecorationIconProperty(flutterParser.InputDecorationIconPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitInputDecorationIconProperty(flutterParser.InputDecorationIconPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTextFieldOnChangedProperty(flutterParser.TextFieldOnChangedPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTextFieldOnChangedProperty(flutterParser.TextFieldOnChangedPropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterImage(flutterParser.ImageContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitImage(flutterParser.ImageContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterImageProperties(flutterParser.ImagePropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitImageProperties(flutterParser.ImagePropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterImageproperty(flutterParser.ImagepropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitImageproperty(flutterParser.ImagepropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterHeight(flutterParser.HeightContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitHeight(flutterParser.HeightContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterWidth(flutterParser.WidthContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitWidth(flutterParser.WidthContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterContainer(flutterParser.ContainerContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitContainer(flutterParser.ContainerContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterContainerproperties(flutterParser.ContainerpropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitContainerproperties(flutterParser.ContainerpropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterChild(flutterParser.ChildContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitChild(flutterParser.ChildContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterColor(flutterParser.ColorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitColor(flutterParser.ColorContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterDecoration(flutterParser.DecorationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitDecoration(flutterParser.DecorationContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterShape(flutterParser.ShapeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitShape(flutterParser.ShapeContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRow(flutterParser.RowContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRow(flutterParser.RowContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRowProperties(flutterParser.RowPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRowProperties(flutterParser.RowPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterRowproperty(flutterParser.RowpropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitRowproperty(flutterParser.RowpropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterChildren(flutterParser.ChildrenContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitChildren(flutterParser.ChildrenContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterColumn(flutterParser.ColumnContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitColumn(flutterParser.ColumnContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterColumnProperties(flutterParser.ColumnPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitColumnProperties(flutterParser.ColumnPropertiesContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterColumnproperty(flutterParser.ColumnpropertyContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitColumnproperty(flutterParser.ColumnpropertyContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ gen/flutterParserListener.java
@@ -0,0 +1,319 @@
+// Generated from C:/Users/Bayan/Desktop/CompilerProject3/src\flutterParser.g4 by ANTLR 4.10.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link flutterParser}.
+ */
+public interface flutterParserListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#prog}.
+	 * @param ctx the parse tree
+	 */
+	void enterProg(flutterParser.ProgContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#prog}.
+	 * @param ctx the parse tree
+	 */
+	void exitProg(flutterParser.ProgContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#widgets}.
+	 * @param ctx the parse tree
+	 */
+	void enterWidgets(flutterParser.WidgetsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#widgets}.
+	 * @param ctx the parse tree
+	 */
+	void exitWidgets(flutterParser.WidgetsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#textField}.
+	 * @param ctx the parse tree
+	 */
+	void enterTextField(flutterParser.TextFieldContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#textField}.
+	 * @param ctx the parse tree
+	 */
+	void exitTextField(flutterParser.TextFieldContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#textFieldProperties}.
+	 * @param ctx the parse tree
+	 */
+	void enterTextFieldProperties(flutterParser.TextFieldPropertiesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#textFieldProperties}.
+	 * @param ctx the parse tree
+	 */
+	void exitTextFieldProperties(flutterParser.TextFieldPropertiesContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#textFieldTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterTextFieldTextProperty(flutterParser.TextFieldTextPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#textFieldTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitTextFieldTextProperty(flutterParser.TextFieldTextPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#textFieldControllerProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterTextFieldControllerProperty(flutterParser.TextFieldControllerPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#textFieldControllerProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitTextFieldControllerProperty(flutterParser.TextFieldControllerPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#textFieldDecorationProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterTextFieldDecorationProperty(flutterParser.TextFieldDecorationPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#textFieldDecorationProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitTextFieldDecorationProperty(flutterParser.TextFieldDecorationPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#inputDecorationProperties}.
+	 * @param ctx the parse tree
+	 */
+	void enterInputDecorationProperties(flutterParser.InputDecorationPropertiesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#inputDecorationProperties}.
+	 * @param ctx the parse tree
+	 */
+	void exitInputDecorationProperties(flutterParser.InputDecorationPropertiesContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#inputDecorationLabelTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterInputDecorationLabelTextProperty(flutterParser.InputDecorationLabelTextPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#inputDecorationLabelTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitInputDecorationLabelTextProperty(flutterParser.InputDecorationLabelTextPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#inputDecorationHintTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterInputDecorationHintTextProperty(flutterParser.InputDecorationHintTextPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#inputDecorationHintTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitInputDecorationHintTextProperty(flutterParser.InputDecorationHintTextPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#inputDecorationHelperTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterInputDecorationHelperTextProperty(flutterParser.InputDecorationHelperTextPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#inputDecorationHelperTextProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitInputDecorationHelperTextProperty(flutterParser.InputDecorationHelperTextPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#inputDecorationIconProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterInputDecorationIconProperty(flutterParser.InputDecorationIconPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#inputDecorationIconProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitInputDecorationIconProperty(flutterParser.InputDecorationIconPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#textFieldOnChangedProperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterTextFieldOnChangedProperty(flutterParser.TextFieldOnChangedPropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#textFieldOnChangedProperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitTextFieldOnChangedProperty(flutterParser.TextFieldOnChangedPropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#image}.
+	 * @param ctx the parse tree
+	 */
+	void enterImage(flutterParser.ImageContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#image}.
+	 * @param ctx the parse tree
+	 */
+	void exitImage(flutterParser.ImageContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#imageProperties}.
+	 * @param ctx the parse tree
+	 */
+	void enterImageProperties(flutterParser.ImagePropertiesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#imageProperties}.
+	 * @param ctx the parse tree
+	 */
+	void exitImageProperties(flutterParser.ImagePropertiesContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#imageproperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterImageproperty(flutterParser.ImagepropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#imageproperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitImageproperty(flutterParser.ImagepropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#height}.
+	 * @param ctx the parse tree
+	 */
+	void enterHeight(flutterParser.HeightContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#height}.
+	 * @param ctx the parse tree
+	 */
+	void exitHeight(flutterParser.HeightContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#width}.
+	 * @param ctx the parse tree
+	 */
+	void enterWidth(flutterParser.WidthContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#width}.
+	 * @param ctx the parse tree
+	 */
+	void exitWidth(flutterParser.WidthContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#container}.
+	 * @param ctx the parse tree
+	 */
+	void enterContainer(flutterParser.ContainerContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#container}.
+	 * @param ctx the parse tree
+	 */
+	void exitContainer(flutterParser.ContainerContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#containerproperties}.
+	 * @param ctx the parse tree
+	 */
+	void enterContainerproperties(flutterParser.ContainerpropertiesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#containerproperties}.
+	 * @param ctx the parse tree
+	 */
+	void exitContainerproperties(flutterParser.ContainerpropertiesContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#child}.
+	 * @param ctx the parse tree
+	 */
+	void enterChild(flutterParser.ChildContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#child}.
+	 * @param ctx the parse tree
+	 */
+	void exitChild(flutterParser.ChildContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#color}.
+	 * @param ctx the parse tree
+	 */
+	void enterColor(flutterParser.ColorContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#color}.
+	 * @param ctx the parse tree
+	 */
+	void exitColor(flutterParser.ColorContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#decoration}.
+	 * @param ctx the parse tree
+	 */
+	void enterDecoration(flutterParser.DecorationContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#decoration}.
+	 * @param ctx the parse tree
+	 */
+	void exitDecoration(flutterParser.DecorationContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#shape}.
+	 * @param ctx the parse tree
+	 */
+	void enterShape(flutterParser.ShapeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#shape}.
+	 * @param ctx the parse tree
+	 */
+	void exitShape(flutterParser.ShapeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#row}.
+	 * @param ctx the parse tree
+	 */
+	void enterRow(flutterParser.RowContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#row}.
+	 * @param ctx the parse tree
+	 */
+	void exitRow(flutterParser.RowContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#rowProperties}.
+	 * @param ctx the parse tree
+	 */
+	void enterRowProperties(flutterParser.RowPropertiesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#rowProperties}.
+	 * @param ctx the parse tree
+	 */
+	void exitRowProperties(flutterParser.RowPropertiesContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#rowproperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterRowproperty(flutterParser.RowpropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#rowproperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitRowproperty(flutterParser.RowpropertyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#children}.
+	 * @param ctx the parse tree
+	 */
+	void enterChildren(flutterParser.ChildrenContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#children}.
+	 * @param ctx the parse tree
+	 */
+	void exitChildren(flutterParser.ChildrenContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#column}.
+	 * @param ctx the parse tree
+	 */
+	void enterColumn(flutterParser.ColumnContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#column}.
+	 * @param ctx the parse tree
+	 */
+	void exitColumn(flutterParser.ColumnContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#columnProperties}.
+	 * @param ctx the parse tree
+	 */
+	void enterColumnProperties(flutterParser.ColumnPropertiesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#columnProperties}.
+	 * @param ctx the parse tree
+	 */
+	void exitColumnProperties(flutterParser.ColumnPropertiesContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link flutterParser#columnproperty}.
+	 * @param ctx the parse tree
+	 */
+	void enterColumnproperty(flutterParser.ColumnpropertyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link flutterParser#columnproperty}.
+	 * @param ctx the parse tree
+	 */
+	void exitColumnproperty(flutterParser.ColumnpropertyContext ctx);
+}
